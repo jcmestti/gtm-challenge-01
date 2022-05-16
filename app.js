7859d3c686d9e2d92fd2272aef166f71baeb3b58
jcmestti <jcmestti@gmail.com>
2022-05-16 15:42:33 -0300

    Adding Nodejs deployment

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const btnRedirect = document.querySelector('.btn-redirect');
+var PORT = 5000;
+var express = require('express');
+var app = express();
 
-btnRedirect.addEventListener('click', () => {
-    //add data to dataLayer
-    console.log('clicked');
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {root: __dirname});
+})
+
+app.get('/redirect.html', function (req, res) {
+    res.sendFile('redirect.html', {root: __dirname});
+})  
+
+var http = require('http');
+var server = http.Server(app);
+
+server.listen(PORT, function() {
+    console.log('App running');
 });
---
+++ index.js
@@ -0,0 +1,6 @@
+const btnRedirect = document.querySelector('.btn-redirect');
+
+btnRedirect.addEventListener('click', () => {
+    //add data to dataLayer
+    console.log('clicked');
+});
